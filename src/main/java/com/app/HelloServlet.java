d7d16bacb420168ff68914a6dcd614eb5315292f
Mateusz Narozny <mat.narozny95@gmail.com>
2020-05-01 20:52:56 +0300

    jetty embeded

--- src/main/java/com/app/HelloServlet.java
+++ src/main/java/com/app/HelloServlet.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -16,9 +17,16 @@ import java.io.IOException;
 public class HelloServlet extends HttpServlet {
 
     private final Logger logger = LoggerFactory.getLogger(HelloServlet.class);
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        logger.info("Req got");
-        resp.getWriter().write("Hello world");
+        String params = req.getParameter("name");
+        if (params == null) {
+            resp.getWriter().write("Hello world");
+        } else {
+            resp.getWriter().write("Hello " + params);
+        }
+        req.getServletContext();
+
     }
 }
